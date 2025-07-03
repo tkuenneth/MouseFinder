ca01f2394b6aa51a98b2a7a35702b7b3a307087e
Thomas Künneth <kuennetht@gmail.com>
2025-07-03 17:03:22 +0200

    Beautify the mouse spot

--- composeApp/src/desktopMain/kotlin/dev/tkuenneth/mousefinder/MouseSpot.kt
+++ composeApp/src/desktopMain/kotlin/dev/tkuenneth/mousefinder/MouseSpot.kt
@@ -1,5 +1,6 @@
 package dev.tkuenneth.mousefinder
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -8,9 +9,12 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.DpSize
@@ -33,6 +37,8 @@ fun MouseSpot(visible: Boolean, position: DpOffset, size: Dp, onCloseRequest: ()
             undecorated = true,
             transparent = true
         ) {
+            val density = LocalDensity.current
+            val rectangleColor = MaterialTheme.colorScheme.onBackground
             Box(
                 modifier = Modifier
                     .fillMaxSize()
@@ -42,7 +48,7 @@ fun MouseSpot(visible: Boolean, position: DpOffset, size: Dp, onCloseRequest: ()
                     )
                     .border(
                         width = 8.dp,
-                        color = MaterialTheme.colorScheme.onBackground,
+                        color = rectangleColor,
                         shape = CircleShape
                     )
                     .pointerInput(PointerEventPass.Main) {
@@ -56,7 +62,52 @@ fun MouseSpot(visible: Boolean, position: DpOffset, size: Dp, onCloseRequest: ()
                             }
                         }
                     }
-            )
+            ) {
+                Canvas(modifier = Modifier.fillMaxSize()) {
+                    val centerX = this.size.width / 2
+                    val centerY = this.size.height / 2
+                    val rectangleLength = with(density) { (size / 4).toPx() }
+                    val rectangleWidth = with(density) { 8.dp.toPx() }
+                    val halfRectangleWidth = rectangleWidth / 2
+                    val offset = with(density) { (size / 8).toPx() }
+
+                    drawRect(
+                        color = rectangleColor,
+                        topLeft = Offset(
+                            x = centerX - halfRectangleWidth,
+                            y = centerY - rectangleLength - offset
+                        ),
+                        size = Size(rectangleWidth, rectangleLength)
+                    )
+
+                    drawRect(
+                        color = rectangleColor,
+                        topLeft = Offset(
+                            x = centerX - halfRectangleWidth,
+                            y = centerY + offset
+                        ),
+                        size = Size(rectangleWidth, rectangleLength)
+                    )
+
+                    drawRect(
+                        color = rectangleColor,
+                        topLeft = Offset(
+                            x = centerX - rectangleLength - offset,
+                            y = centerY - halfRectangleWidth
+                        ),
+                        size = Size(rectangleLength, rectangleWidth)
+                    )
+
+                    drawRect(
+                        color = rectangleColor,
+                        topLeft = Offset(
+                            x = centerX + offset,
+                            y = centerY - halfRectangleWidth
+                        ),
+                        size = Size(rectangleLength, rectangleWidth)
+                    )
+                }
+            }
         }
     }
 }
