387106876e4d2cd98f67e40b810dc1a353495ffe
Thomas Künneth <kuennetht@gmail.com>
2025-07-03 15:24:21 +0200

    Refactoring

---
+++ composeApp/src/desktopMain/kotlin/dev/tkuenneth/mousefinder/MouseSpot.kt
@@ -0,0 +1,55 @@
+package dev.tkuenneth.mousefinder
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.rememberWindowState
+
+
+@Composable
+fun MouseSpot(visible: Boolean, position: DpOffset, size: Dp, onCloseRequest: () -> Unit) {
+    if (visible) {
+        Window(
+            onCloseRequest = onCloseRequest,
+            state = rememberWindowState(
+                size = DpSize(width = size, height = size),
+                position = WindowPosition(x = position.x, y = position.y),
+            ),
+            alwaysOnTop = true,
+            undecorated = true,
+            transparent = true
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = MaterialTheme.colorScheme.background,
+                        shape = CircleShape
+                    )
+                    .pointerInput(PointerEventPass.Main) {
+                        awaitPointerEventScope {
+                            while (true) {
+                                val event = awaitPointerEvent()
+                                when (event.type) {
+                                    PointerEventType.Exit, PointerEventType.Press ->
+                                        onCloseRequest()
+                                }
+                            }
+                        }
+                    }
+            )
+        }
+    }
+}
--- composeApp/src/desktopMain/kotlin/dev/tkuenneth/mousefinder/main.kt
+++ composeApp/src/desktopMain/kotlin/dev/tkuenneth/mousefinder/main.kt
@@ -1,29 +1,14 @@
 package dev.tkuenneth.mousefinder
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpOffset
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
 import java.awt.MouseInfo
 
 fun main() = application {
@@ -81,40 +66,4 @@ fun main() = application {
             allowShortcuts = true
         }
     )
-}
-
-@Composable
-fun MouseSpot(visible: Boolean, position: DpOffset, size: Dp, onCloseRequest: () -> Unit) {
-    if (visible) {
-        Window(
-            onCloseRequest = onCloseRequest,
-            state = rememberWindowState(
-                size = DpSize(width = size, height = size),
-                position = WindowPosition(x = position.x, y = position.y),
-            ),
-            alwaysOnTop = true,
-            undecorated = true,
-            transparent = true
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        color = MaterialTheme.colorScheme.background,
-                        shape = CircleShape
-                    )
-                    .pointerInput(PointerEventPass.Main) {
-                        awaitPointerEventScope {
-                            while (true) {
-                                val event = awaitPointerEvent()
-                                when (event.type) {
-                                    PointerEventType.Exit, PointerEventType.Press ->
-                                        onCloseRequest()
-                                }
-                            }
-                        }
-                    }
-            )
-        }
-    }
 }
