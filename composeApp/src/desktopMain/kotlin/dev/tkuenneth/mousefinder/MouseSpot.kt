bdbd252b43e853a99933a07cc427dc954cc9268e
Thomas Künneth <kuennetht@gmail.com>
2025-07-03 15:36:42 +0200

    Add a border for improved visibility

--- composeApp/src/desktopMain/kotlin/dev/tkuenneth/mousefinder/MouseSpot.kt
+++ composeApp/src/desktopMain/kotlin/dev/tkuenneth/mousefinder/MouseSpot.kt
@@ -1,6 +1,7 @@
 package dev.tkuenneth.mousefinder
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CircleShape
@@ -13,6 +14,7 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.rememberWindowState
@@ -36,6 +38,11 @@ fun MouseSpot(visible: Boolean, position: DpOffset, size: Dp, onCloseRequest: ()
                     .fillMaxSize()
                     .background(
                         color = MaterialTheme.colorScheme.background,
+                        shape = CircleShape
+                    )
+                    .border(
+                        width = 8.dp,
+                        color = MaterialTheme.colorScheme.onBackground,
                         shape = CircleShape
                     )
                     .pointerInput(PointerEventPass.Main) {
